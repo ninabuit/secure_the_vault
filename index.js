d655cf3a065dde4de140eb8ea34117ce4c1a64a3
Nina <your_email@example.comNina>
2024-12-05 20:19:47 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+//I need to set three variables using a calculation
+//I need the variables to result in the answer to the calculation
+//I need to create a string for the user saying "You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
+//I need to create a dialogue box displaying the vault code 
+
+// create calculation that will result in 10, 40, 39
+
+//first calc
+const firstCalc = 5 * 2;
+
+//second calc
+const secondCalc = 20 + 20;
+
+//third calc
+const thirdCalc = 40 - 1;
+
+// create display message
+const theMessage = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
+
+// create dialoge box
+alert (theMessage + firstCalc + "," + secondCalc + "," + thirdCalc)
+
+// message discribing what this is 
+console.log ("block 7 workshop")
